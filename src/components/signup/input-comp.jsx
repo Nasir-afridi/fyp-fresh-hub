503288a048c9e46914281158e1e0aa8b4c99f38f
Nasir-afridi <nasirwc05@gmail.com>
2025-02-02 15:34:27 +0500

    input-comp changes added

--- src/components/signup/input-comp.jsx
+++ src/components/signup/input-comp.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 const InputForm = () => {
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    pasword: "",
+  });
+
+  const onChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <>
       <div className="heading-create text-center mt-5">
@@ -7,9 +24,9 @@ const InputForm = () => {
       <div className="car-img text-center mt-5">
         <img src="cars.svg" />
       </div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="mb-4">
-          <label for="fullName" className="form-label">
+          <label htmlFor="fullName" className="form-label">
             First Name
           </label>
           <input
@@ -17,11 +34,13 @@ const InputForm = () => {
             className="form-control"
             id="fullName"
             placeholder="First Name"
+            name="first_name"
+            onChange={onChange}
             required
           />
         </div>
         <div className="mb-4">
-          <label for="phoneNumber" className="form-label">
+          <label htmlFor="phoneNumber" className="form-label">
             Last Name
           </label>
           <input
@@ -29,11 +48,13 @@ const InputForm = () => {
             className="form-control"
             id="phoneNumber"
             placeholder="Last Name"
+            name="last_name"
+            onChange={onChange}
             required
           />
         </div>
         <div className="mb-4">
-          <label for="email" className="form-label">
+          <label htmlFor="email" className="form-label">
             E-mail
           </label>
           <input
@@ -41,25 +62,31 @@ const InputForm = () => {
             className="form-control"
             id="email"
             placeholder="E-mail"
+            name="email"
+            onChange={onChange}
           />
         </div>
         <div className="mb-4">
-          <label for="email" className="form-label">
+          <label htmlFor="email" className="form-label">
             Password
           </label>
           <input
-            type="email"
+            type="password"
             className="form-control"
-            id="email"
+            id="password"
             placeholder="Password"
+            name="password"
+            onChange={onChange}
           />
         </div>
         <div className="d-grid">
-          <button className="Next">Next</button>
+          <button type="submit" className="Next">
+            Next
+          </button>
         </div>
 
         <p className="footer-text mt-4">
-          By creating an account, you are agreeing to our{" "}
+          By creating an account, you are agreeing to our
           <a href="#">User Agreement</a> and <a href="#">Privacy Policy.</a>.
         </p>
       </form>
