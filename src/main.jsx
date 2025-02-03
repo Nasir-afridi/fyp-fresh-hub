99160a1de0dbb1c86e45d568c924fe14ec2c15fe
Nasir-afridi <nasirwc05@gmail.com>
2025-02-03 17:29:53 +0500

    import the store and its provider

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "font-awesome/css/font-awesome.min.css";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </Provider>
 );
