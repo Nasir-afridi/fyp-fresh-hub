075870ecbc3b840c3467a3bfc59f0d2a20bd2852
Nasir-afridi <nasirwc05@gmail.com>
2025-02-08 13:37:19 +0500

    added the logout functionality when user click on logout the token will removed and user navigate to login

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutUser } from "../../features/userSlice";
 
 const TopNav = () => {
+  const dispatch = useDispatch();
   const { token } = useSelector((state) => state.user);
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
 
   return (
     <nav className="navbar navbar-expand-lg sticky-top nav-bar">
@@ -58,7 +64,11 @@ const TopNav = () => {
           </ul>
 
           {token && (
-            <button type="button" className="btn btn-danger me-2">
+            <button
+              type="button"
+              className="btn btn-danger ms-auto"
+              onClick={handleLogout}
+            >
               Logout
             </button>
           )}
