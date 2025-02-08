9a94a33271088cf17e9f21f8a745219bf2d2baf1
Nasir-afridi <nasirwc05@gmail.com>
2025-02-08 13:15:03 +0500

    private and public routes working successfully

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+// singup
 export const submitForm = createAsyncThunk(
   'user/submitForm',
   async (formData, { rejectWithValue }) => {
@@ -13,26 +14,75 @@ export const submitForm = createAsyncThunk(
   }
 );
 
+// login
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (userData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post('http://localhost:4000/registration/login', userData);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const logoutUser = () => {
+  return (dispatch) => {
+    localStorage.removeItem("token"); 
+    dispatch(logout());
+  };
+};
+
 const userSlice = createSlice({
   name: 'user',
   initialState: {
+    user: null,
+    token: localStorage.getItem('token'),
     status: 'idle',
     error: null,
   },
-  reducers: {},
+  reducers: {
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      localStorage.removeItem('token'); 
+    },
+  },
   extraReducers: (builder) => {
     builder
+      // For Signup
       .addCase(submitForm.pending, (state) => {
         state.status = 'loading';
       })
-      .addCase(submitForm.fulfilled, (state) => {
+      .addCase(submitForm.fulfilled, (state, action) => {
+        console.log(action)
         state.status = 'succeeded';
+        state.user = action.payload?.user || {};
       })
       .addCase(submitForm.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.payload;
+      })
+      
+      // For Login
+      .addCase(loginUser.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.user = action.payload.user; 
+        state.token = action.payload.token; 
+        localStorage.setItem('token', action.payload.token);
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload;
       });
+      
   },
 });
+
+export const { logout } = userSlice.actions;
 
 export default userSlice.reducer;
