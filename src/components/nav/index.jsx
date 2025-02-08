8a732f0bf2ea768fa634da08e30c122541e64071
Nasir-afridi <nasirwc05@gmail.com>
2025-02-08 13:14:35 +0500

    import the token

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const TopNav = () => {
+  const { token } = useSelector((state) => state.user);
+
   return (
     <nav className="navbar navbar-expand-lg sticky-top nav-bar">
       <div className="container">
-        Text
+        <span className="navbar-brand">Text</span>
         <button
           className="navbar-toggler"
           type="button"
@@ -17,33 +20,48 @@ const TopNav = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/home">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/pricing">Pricing</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link to="/signup">SignUp</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/login">Login</Link>
-            </li>
+            {token ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/home">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/pricing">
+                    Pricing
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup">
+                    Sign Up
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
-          <div className="navbar-text">
+
+          {token && (
             <button type="button" className="btn btn-danger me-2">
-              Login
+              Logout
             </button>
-            <button type="button" className="btn btn-warning">
-              Sign-Up
-            </button>
-          </div>
+          )}
         </div>
       </div>
     </nav>
