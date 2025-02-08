4480c6b18acb1f01ac6252655a573176905895b4
Nasir-afridi <nasirwc05@gmail.com>
2025-02-08 11:43:03 +0500

    hide the login signup and home pages

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../../features/userSlice";
 
 const TopNav = () => {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.user);
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
   return (
     <nav className="navbar navbar-expand-lg sticky-top nav-bar">
       <div className="container">
-        <a className="navbar-brand" href="#">
-          Text
-        </a>
+        Text
         <button
           className="navbar-toggler"
           type="button"
@@ -21,45 +27,37 @@ const TopNav = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">
-                <Link to="/home">Home</Link>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <Link to="/about">About</Link>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <Link to="/pricing">Pricing</Link>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <Link to="/signup">SignUp</Link>
-              </a>
-            </li>
+            {token && (
+              <>
+                {" "}
+                <li className="nav-item">
+                  <Link to="/home">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/about">About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/pricing">Pricing</Link>
+                </li>
+              </>
+            )}
+
             {/* <li className="nav-item">
-              <a className="nav-link" href="#">
+             
                 <Link to="/otp">Otp</Link>
-              </a>
+              
             </li> */}
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <Link to="/login">Login</Link>
-              </a>
-            </li>
+            {!token && (
+              <>
+                <li className="nav-item">
+                  <Link to="/signup">SignUp</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/login">Login</Link>
+                </li>
+              </>
+            )}
           </ul>
-          <div className="navbar-text">
-            <button type="button" className="btn btn-danger me-2">
-              Login
-            </button>
-            <button type="button" className="btn btn-warning">
-              Sign-Up
-            </button>
-          </div>
         </div>
       </div>
     </nav>
